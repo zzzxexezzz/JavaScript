bf6ce752dd57276251be2d3db5194893e6f59c5e
Andrey <it.abramov.a@gmail.com>
2019-04-03 21:41:19 +1000

    First pull

--- React/my-app/src/App.js
+++ React/my-app/src/App.js
@@ -1,29 +1,20 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
-import Header from './Header.js';
-import Tehnol from './Technolog.js';
+import Header from './components/Header/Header';
+import Nav from './components/Nav/Nav';
+import Profile from './components/Profile/Profile';
 
 
 const App = () => {
-  return (
-    <div className="App">
-    <Header/>
-    <Tehnol/>
-      Test
-       <ul>
-        <li>test</li>
-        <li>test</li>
-        <li>test</li>
-      </ul>
-  </div>
-  );
+    return (
+        < div
+            className="app-wrapper">
+            < Header/>
+            < Nav/>
+            < Profile/>
+        </div>
+    )
+        ;
 
 }
-
-
-
-
-
-
 export default App;
