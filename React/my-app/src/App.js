3eb357cadd378a8868bd17a021f6b3e01cb56840
Andrey <it.abramov.a@gmail.com>
2019-04-04 21:03:29 +1000

    04042019

--- React/my-app/src/App.js
+++ React/my-app/src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import './App.css';
 import Header from './components/Header/Header';
 import Nav from './components/Nav/Nav';
+import Dialogs from "./components/Dialogs/Dialogs";
 import Profile from './components/Profile/Profile';
 
 
@@ -11,10 +12,12 @@ const App = () => {
             className="app-wrapper">
             < Header/>
             < Nav/>
-            < Profile/>
+            <div className='app-wrapper-content'>
+                <Dialogs/>
+                {/*<Profile />*/}
+            </div>
+            {/*< Profile/>*/}
         </div>
-    )
-        ;
-
+    );
 }
 export default App;
--- React/my-app/src/components/Profile/Profile.jsx
+++ React/my-app/src/components/Profile/Profile.jsx
@@ -4,7 +4,7 @@ import Post from './Posts/Posts';
 
 const Profile =()=>{
     return(
-        <div className={classes.content}>
+        <div>
         <div>
             <img src="https://www.nastol.com.ua/pic/201209/1920x1200/nastol.com.ua-32997.jpg" alt=''/></div>
         <div>
