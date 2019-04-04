34c9534fb100e9889dc59394db25e8220a223e73
Andrey <it.abramov.a@gmail.com>
2019-04-04 22:41:20 +1000

    04042019_3

--- React/my-app/src/components/Nav/Nav.jsx
+++ React/my-app/src/components/Nav/Nav.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import classes from './Nav.module.css';
+import {NavLink} from "react-router-dom";
 
 const Nav = () =>{
-    return(
+    return (
         <nav className={classes.nav}>
-            <div className={classes.item}><a href="/profile">Profile</a></div>
-            <div className={classes.item}><a href="/dialogs">Message</a></div>
-            <div className={classes.item}><a href="/news">News</a></div>
-            <div className={classes.item}><a href="/musick">Music</a></div>
-            <div className={classes.item}><a href="/seting">Seting</a></div>
-      </nav>
+            <div className={classes.item}><NavLink to="/profile">Profile</NavLink></div>
+            <div className={classes.item}><NavLink to="/dialogs">Message</NavLink></div>
+            <div className={classes.item}><NavLink to="/news">News</NavLink></div>
+            <div className={classes.item}><NavLink to="/musick">Music</NavLink></div>
+            <div className={classes.item}><NavLink to="/seting">Seting</NavLink></div>
+        </nav>
     )
 }
 
