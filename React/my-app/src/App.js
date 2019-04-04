b9e4dd9a57bd6a9556219c4ecc1f762e5cc8412e
Andrey <it.abramov.a@gmail.com>
2019-04-04 21:57:59 +1000

    04042019_2

--- React/my-app/src/App.js
+++ React/my-app/src/App.js
@@ -4,20 +4,22 @@ import Header from './components/Header/Header';
 import Nav from './components/Nav/Nav';
 import Dialogs from "./components/Dialogs/Dialogs";
 import Profile from './components/Profile/Profile';
+import {BrowserRouter, Route} from "react-router-dom";
 
 
-const App = () => {
-    return (
-        < div
-            className="app-wrapper">
-            < Header/>
-            < Nav/>
-            <div className='app-wrapper-content'>
-                <Dialogs/>
-                {/*<Profile />*/}
+const App = (props) => {
+    return(
+    <BrowserRouter>
+            <div
+                className="app-wrapper">
+                <Header/>
+                <Nav/>
+                <div className='app-wrapper-content'>
+                    <Route path ='/dialog' component={Dialogs}/>
+                    <Route path='/profile' component={Profile}/>
+                </div>
             </div>
-            {/*< Profile/>*/}
-        </div>
+    </BrowserRouter>
     );
 }
 export default App;
--- React/my-app/src/components/Profile/Profile.jsx
+++ React/my-app/src/components/Profile/Profile.jsx
@@ -6,7 +6,7 @@ const Profile =()=>{
     return(
         <div>
         <div>
-            <img src="https://www.nastol.com.ua/pic/201209/1920x1200/nastol.com.ua-32997.jpg" alt=''/></div>
+            <img src="https://www.nastol.com.ua/pic/201209/1920x1200/nastol.com.ua-32997.jpg" width='40' alt=''/></div>
         <div>
           ava+discripteon
         </div>
