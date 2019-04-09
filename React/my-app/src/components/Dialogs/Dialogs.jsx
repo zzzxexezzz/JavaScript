b9c0d00e5e11e9ff1e0cd4f7c2b9e97d45f5347a
Andrey <it.abramov.a@gmail.com>
2019-04-09 22:23:43 +1000

    09042019

--- React/my-app/src/components/Dialogs/Dialogs.jsx
+++ React/my-app/src/components/Dialogs/Dialogs.jsx
@@ -3,24 +3,37 @@ import classes from './Dialogs.module.css'
 import {NavLink} from "react-router-dom";
 
 const DialogsItem = (props) =>{
-    return<div>
-        <NavLink to="/dialogs/1">{props.name}</NavLink>
+    let path="/dialogs/"+ props.id;
+    return<div className={classes.dialog + ' ' + classes.active}>
+        <NavLink to={path}>{props.name}</NavLink>
     </div>
 }
 
 const Dialogs =(props)=>{
     return <div className={classes.dialogs}>
                 <div className={classes.dialogsItems}>
+
+                    <DialogsItem name="Dmitriy" id="1"/>
+                    <DialogsItem name="Misha" id="2"/>
+                    <DialogsItem name="Sasha" id="3"/>
+                    <DialogsItem name="Andrey" id="4"/>
+                    <DialogsItem name="Sergey" id="5"/>
+                    <DialogsItem name="Anton" id="6"/>
+
+                {/*
+
                     <div className={classes.dialog}>
                         <NavLink to="/dialogs/1">Dmitriy</NavLink>
                     </div>
+
+
                     <div className={classes.dialog}>
                         <NavLink to="/dialogs/2">Misha</NavLink>
                     </div>
                     <div className={classes.dialog}><NavLink to="/dialogs/3">Sasha</NavLink></div>
                     <div className={classes.dialog}><NavLink to="/dialogs/4">Andrey</NavLink></div>
                     <div className={classes.dialog}><NavLink to="/dialogs/5">Sergey</NavLink></div>
-                    <div className={classes.dialog}><NavLink to="/dialogs/6">Anton</NavLink></div>
+                    <div className={classes.dialog}><NavLink to="/dialogs/6">Anton</NavLink></div>*/}
 
                 </div>
                 <div className={classes.messages}>
